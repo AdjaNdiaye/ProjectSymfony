a3db411d7f0b43638beca78ca79e9254eb3f914f
Adja Ndiaye <adjandiaye242@gmail.com>
2024-10-10 11:35:47 +0200

    add

--- src/Security/LoginAutrhenticatorAuthenticator.php
+++ src/Security/LoginAutrhenticatorAuthenticator.php
@@ -46,8 +46,9 @@ class LoginAutrhenticatorAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        
+        return new RedirectResponse($this->urlGenerator->generate('app_test'));
+       // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
