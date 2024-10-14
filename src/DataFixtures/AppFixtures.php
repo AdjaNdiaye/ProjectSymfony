5cdb770cd42e35de5fc1b632b40faf3a1695d261
Adja Ndiaye <adjandiaye242@gmail.com>
2024-10-14 09:31:32 +0200

    add

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,14 +5,25 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $encoder;
+public function __construct(UserPasswordHasherInterface $encoder)
+{
+    $this->encoder = $encoder; 
+ }
     public function load(ObjectManager $manager): void
     {
          $user = new User();
-        // $manager->persist($product);
+         $user->setNom('ndiaye')->setPrenom('adja');
+         $user->setEmail('adja/ndiaye@gmail.com');
+         $encoded = $this->encoder->hashpassword($user,'123');
+         $user->setPassword($encoded);
+        $user->setRoles(['ROLE_USER']);
 
+        $manager->persist($user);
         $manager->flush();
     }
 }
